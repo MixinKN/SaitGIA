2285938f8659f212c5c80a88500845d162aa5ffe
MixinKN <mixin.kn@gmail.com>
2025-11-01 01:39:57 +0300

    push6

--- src/types/catalogMenu.ts
+++ src/types/catalogMenu.ts
@@ -10,25 +10,25 @@ interface CatalogMenu {
 export const CATALOG_MENU_1: CatalogMenu[] = [
     {
         id: 1,
-        image: "url(./subject_eng1.png)",
+        image: "url(/SaitGIA/subject_eng1.png)",
         title: "Английский язык",
         price: "От 3784 руб",
     },
     {
         id: 2,
-        image: "url(./subject_bio1.png)",
+        image: "url(/SaitGIA/subject_bio1.png)",
         title: "Биология",
         price: "От 4632 руб",
     },
     {
         id: 3,
-        image: "url(./subject_geo1.png)",
+        image: "url(/SaitGIA/subject_geo1.png)",
         title: "География",
         price: "От 2321 руб",
     },
     {
         id: 4,
-        image: "url(./subject_inf1.png)",
+        image: "url(/SaitGIA/subject_inf1.png)",
         title: "Информатика и ИКТ",
         price: "От 5643 руб",
     },
@@ -36,25 +36,25 @@ export const CATALOG_MENU_1: CatalogMenu[] = [
 export const CATALOG_MENU_2: CatalogMenu[] = [
     {
         id: 1,
-        image: "url(./subject_esp1.png)",
+        image: "url(/SaitGIA/subject_esp1.png)",
         title: "Испанский язык",
         price: "От 3109 руб",
     },
     {
         id: 2,
-        image: "url(./subject_his1.png)",
+        image: "url(/SaitGIA/subject_his1.png)",
         title: "История",
         price: "От 2395 руб",
     },
     {
         id: 3,
-        image: "url(./subject_oth1.png)",
+        image: "url(/SaitGIA/subject_oth1.png)",
         title: "Китайский язык",
         price: "От 6538 руб",
     },
     {
         id: 4,
-        image: "url(./subject_lit1.png)",
+        image: "url(/SaitGIA/subject_lit1.png)",
         title: "Литература",
         price: "От 7639 руб",
     },
@@ -62,25 +62,25 @@ export const CATALOG_MENU_2: CatalogMenu[] = [
 export const CATALOG_MENU_3: CatalogMenu[] = [
     {
         id: 1,
-        image: "url(./subject_oth1.png)",
+        image: "url(/SaitGIA/subject_oth1.png)",
         title: "Математика базовый уровень",
         price: "От 1895 руб",
     },
     {
         id: 2,
-        image: "url(./subject_oth1.png)",
+        image: "url(/SaitGIA/subject_oth1.png)",
         title: "Математика профильный уровень",
         price: "От 4569 руб",
     },
     {
         id: 3,
-        image: "url(./subject_ger1.png)",
+        image: "url(/SaitGIA/subject_ger1.png)",
         title: "Немецкий язык",
         price: "От 3017 руб",
     },
     {
         id: 4,
-        image: "url(./subject_soc1.png)",
+        image: "url(/SaitGIA/subject_soc1.png)",
         title: "Обществознание",
         price: "От 4127 руб",
     },
@@ -88,25 +88,25 @@ export const CATALOG_MENU_3: CatalogMenu[] = [
 export const CATALOG_MENU_4: CatalogMenu[] = [
     {
         id: 1,
-        image: "url(./subject_oth1.png)",
+        image: "url(/SaitGIA/subject_oth1.png)",
         title: "Русский язык",
         price: "От 2896 руб",
     },
     {
         id: 2,
-        image: "url(./subject_phs1.png)",
+        image: "url(/SaitGIA/subject_phs1.png)",
         title: "Физика",
         price: "От 3421 руб",
     },
     {
         id: 3,
-        image: "url(./subject_fra1.png)",
+        image: "url(/SaitGIA/subject_fra1.png)",
         title: "Французский язык",
         price: "От 2854 руб",
     },
     {
         id: 4,
-        image: "url(./subject_him1.png)",
+        image: "url(/SaitGIA/subject_him1.png)",
         title: "Французский язык",
         price: "От 2854 руб",
     },
